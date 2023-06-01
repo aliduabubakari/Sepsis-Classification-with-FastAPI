f7cd12c0c3bbd1bf51929826d97ba749b038fa20
Alidu Abubakari <alidu143@gmail.com>
2023-06-01 23:18:10 +0000

    major update

--- Notebooks/main_fastapi.py
+++ Notebooks/main_fastapi.py
@@ -56,9 +56,19 @@ def predict_sepsis_endpoint(PRG: float, PL: float, PR: float, SK: float, TS: flo
     prediction = np.argmax(probabilities)
 
     sepsis_status = "Positive" if prediction == 1 else "Negative"
+    
     probability = probabilities[1] if prediction == 1 else probabilities[0]
 
-    statement = f"The patient is {sepsis_status}. There is a {'high' if prediction == 1 else 'low'} probability ({probability:.2f}) that the patient is susceptible to developing sepsis."
+    #statement = f"The patient is {sepsis_status}. There is a {'high' if prediction == 1 else 'low'} probability ({probability:.2f}) that the patient is susceptible to developing sepsis."
+
+    if prediction == 1:
+        status_icon = "✘"  # Red 'X' icon for positive sepsis prediction
+        sepsis_explanation = "Sepsis is a life-threatening condition caused by an infection. A positive prediction suggests that the patient might be exhibiting sepsis symptoms and requires immediate medical attention."
+    else:
+        status_icon = "✔"  # Green checkmark icon for negative sepsis prediction
+        sepsis_explanation = "Sepsis is a life-threatening condition caused by an infection. A negative prediction suggests that the patient is not currently exhibiting sepsis symptoms."
+
+    statement = f"The patient's sepsis status is {sepsis_status} {status_icon} with a probability of {probability:.2f}. {sepsis_explanation}"
 
     user_input_statement = "Please note this is the user-inputted data: "
 
